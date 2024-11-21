b0060002b7fb2c64b5e107d8dae4dcc210055af2
Murilo <murilodiasdeoliveira@gmail.com>
2024-11-21 18:59:05 -0300

    inicializando spring

---
+++ Tarefas/src/test/java/Gerenciador/Tarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package Gerenciador.Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
